58023c691273e052086662333358cca12220f05a
lizzzzzy <lizapozdnyakova@gmail.com>
2017-05-18 02:48:06 +0300

    add all mobile / add slider

--- assets/source/scripts/hover.js
+++ assets/source/scripts/hover.js
@@ -9,3 +9,34 @@ $(document).ready(function() {
       .toggleClass("hover", e.type == 'mouseover');
   });
 });
+
+var slider = document.querySelector('.slider-container');
+
+    var slides = document.querySelectorAll('.slider__item');
+    var currentSlide = 0;
+    var slideInterval = setInterval(nextSlide, 20000);
+
+    function nextSlide() {
+      goToSlide(currentSlide + 1);
+    }
+
+    function previousSlide() {
+      goToSlide(currentSlide - 1);
+    }
+
+    function goToSlide(n) {
+      slides[currentSlide].className = 'slider__item';
+      currentSlide = (n + slides.length) % slides.length;
+      slides[currentSlide].className = 'slider__item showing';
+    }
+
+    var next = document.getElementById('next');
+    var previous = document.getElementById('prev');
+
+    next.onclick = function () {
+      nextSlide();
+    };
+    previous.onclick = function () {
+      previousSlide();
+    };
+  
