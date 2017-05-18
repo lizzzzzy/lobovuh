ea41f6ae462da12c6bc06300a607d57a7b35596b
ipacmanx <ipacmanx@gmail.com>
2017-05-18 03:02:29 +0300

    Fix _index

--- index.php
+++ index.php
@@ -1,7 +1,14 @@
 <?php
 
-$html = file_get_contents('index.html');
+$html = file_get_contents('_index.html');
 $data = json_decode(file_get_contents('data/cities.json'));
+
+$cities = '';
+foreach ($data as $city) {
+  $cities .= '<option>'.$city.'</option>';
+}
+$city = $data[0];
 
 $html = str_replace("%cities%", $cities, $html);
 $html = str_replace("%city%", $city, $html);
+echo $html;
